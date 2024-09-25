5a94cbae755db55f2b2f127b25346f7bd82c8609
Olzhas <mrbll@list.ru>
2024-09-25 14:16:06 +0500

    o

--- src/index.ts
+++ src/index.ts
@@ -20,19 +20,7 @@ app.use((req, res, next) => {
 
 // const BASE_URL = "https://www.kazvision.media"
 
-app.use(cors({
-    origin: (origin, callback) => {
-      // Позволяет запросы с 'http://localhost:3001' и 'https://tengri-app.vercel.app'
-      const allowedOrigins = ['http://localhost:3001', 'https://tengri-app.vercel.app'];
-      if (allowedOrigins.includes(origin as string) || !origin) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    credentials: true
-  }));
+app.use(cors());
 
 app.use(express.json());
 
